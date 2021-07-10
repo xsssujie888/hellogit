b4f780e09339443abaf82d943abb8a9570715df2
xsssujie888 <87175219+xsssujie888@users.noreply.github.com>
2021-07-10 16:18:00 +0800

    更新

--- Hellogit/src/main/java/cn/edu/ujn/hellogit.java
+++ Hellogit/src/main/java/cn/edu/ujn/hellogit.java
@@ -4,5 +4,6 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hahaha");
+        System.out.println("hahaha");
     }
 }
